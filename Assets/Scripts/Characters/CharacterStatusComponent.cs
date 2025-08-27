5f0f665ef3f46b50737d9266b02d96dfea33a6bf
Veyro11 <20050620hkchan@gmail.com>
2025-08-28 00:22:38 +0900

    250827 FINAL COMMIT

---
+++ Assets/Scripts/Characters/CharacterStatusComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class CharacterStatusComponent : MonoBehaviour
+{
+    public List<Status> statuses = new List<Status>();
+    
+}
